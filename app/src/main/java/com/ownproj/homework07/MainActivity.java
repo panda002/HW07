189f09dbaeb180382e8244ae8d55e32917a54bf4
Sidharth Panda <32598234+panda002@users.noreply.github.com>
2019-11-17 10:02:34 -0500

    Updated the Main page

--- app/src/main/java/com/ownproj/homework07/MainActivity.java
+++ app/src/main/java/com/ownproj/homework07/MainActivity.java
@@ -1,39 +1,121 @@
 package com.ownproj.homework07;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    Button btn_login;
+    Button btn_signup;
+
+    static final int GOOGLE_SIGN = 123;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btn_login=findViewById(R.id.btn_login);
+        btn_signup=findViewById(R.id.btn_signup);
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                .Builder().requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        btn_login.setOnClickListener(v -> SigninGoogle());
+
+        if(mAuth.getCurrentUser()!= null)
+        {
+            FirebaseUser user = mAuth.getCurrentUser();
+            updateUI(user);
+        }
+
+
+    }
+
+    void SigninGoogle()
+    {
+        Intent signIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent,GOOGLE_SIGN);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GOOGLE_SIGN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn
+                    .getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if(account!= null) firebaseAuthWithGoogle(account);
+            }catch (ApiException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
     }
 
-    private void updateUI(FirebaseUser currentUser) {
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d("TAG", "firebaseAuthWithGoogle: "+account.getId());
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if(task.isSuccessful())
+                    {
+
+                        Log.d("TAG", "SignIn Successful");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    }else
+                    {
+                        Log.d("TAG", "firebaseAuthWithGoogle: "+task.getException());
+                        updateUI(null);
+                    }
+                });
     }
 
+    private void updateUI(FirebaseUser user) {
+        if(user!=null)
+        {
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+
+            Log.d("TAG", "name : "+name);
+            Log.d("TAG", "email : "+email);
+
+        }else{
+
+        }
+    }
 }
 
